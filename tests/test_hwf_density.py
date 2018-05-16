9246456801c7c283d68439d925d9d12f4a1f6fef
Lenson Pellouchoud <lensonp@gmail.com>
2018-05-16 10:03:24 -0700

    some real tests

--- tests/test_hwf_density.py
+++ tests/test_hwf_density.py
@@ -1,12 +1,21 @@
+import os
+
+import numpy as np
+
 from hwaves import hwf_density
 
-nx = 10
-ny = 10
-nz = 10
-dx = 0.02
-dy = 0.02
-dz = 0.02
+tests_path = os.path.join(os.getcwd(),'tests')
 
+nx = 30
+ny = 30
+nz = 30
+dx = 0.2
+dy = 0.2
+dz = 0.2
 
-ijk_xyz_PV = hwf_density.cartesian_density(nx,ny,nz,dx,dy,dz)
+def test_cartesian_density():
+    ijk_xyz_PV = hwf_density.cartesian_density(nx,ny,nz,dx,dy,dz)
+    fpath = os.path.join(tests_path,'cartesian_density_test.dat')
+    hwf_density.write_cartesian(ijk_xyz_PV,fpath)
+    assert(os.path.exists(fpath))
 
