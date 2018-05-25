6aee4e098817d3955236c63e9c81c2816eaf4fd3
Lenson Pellouchoud <lensonp@gmail.com>
2018-05-25 15:41:16 -0700

    test_hwf_density generates densities for n=1,2,3

--- tests/test_hwf_density.py
+++ tests/test_hwf_density.py
@@ -14,8 +14,18 @@ dy = 0.2
 dz = 0.2
 
 def test_cartesian_density():
-    ijk_xyz_PV = hwf_density.cartesian_density(nx,ny,nz,dx,dy,dz)
-    fpath = os.path.join(tests_path,'cartesian_density_test.dat')
+
+    ijk_xyz_PV = hwf_density.cartesian_density(nx,ny,nz,dx,dy,dz,1,1)
+    fpath = os.path.join(tests_path,'cartesian_density_Z1_n1.dat')
     hwf_density.write_cartesian(ijk_xyz_PV,fpath)
+
+    ijk_xyz_PV = hwf_density.cartesian_density(nx,ny,nz,dx,dy,dz,1,2)
+    fpath = os.path.join(tests_path,'cartesian_density_Z1_n2.dat')
+    hwf_density.write_cartesian(ijk_xyz_PV,fpath)
+
+    ijk_xyz_PV = hwf_density.cartesian_density(nx,ny,nz,dx,dy,dz,1,3)
+    fpath = os.path.join(tests_path,'cartesian_density_Z1_n3.dat')
+    hwf_density.write_cartesian(ijk_xyz_PV,fpath)
+
     assert(os.path.exists(fpath))
 
