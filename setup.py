f5cd66b1557fdacc090602cf15135d2771d9f753
Lenson Pellouchoud <lensonp@gmail.com>
2019-07-18 11:09:56 -0700

    version+, added requirements, corrected license

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ longdesc = 'Current capabilities: '\
     'Spherical harmonics; '\
     'Real-space densities of hydrogenic eigenfunctions; '\
     'Real-space density of real-valued wavefunctions; '\
-    'Real-space density isosurfaces and polygon data'
+    'Real-space density isosurfaces and polygon data with .obj output'
 
 setup(
     name='hwaves',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/lensonp/hwaves.git',
     description='Compute and plot hydrogenic wavefunctions',
     long_description=longdesc,
     author='Lenson A. Pellouchoud',
-    license='BSD',
+    license='MIT',
     author_email='',
-    install_requires=['numpy','scipy','masstable','periodictable'],
+    install_requires=['numpy','scipy','masstable','periodictable','matplotlib','scikit-image'],
     packages=find_packages(),
     package_data={}
     )
